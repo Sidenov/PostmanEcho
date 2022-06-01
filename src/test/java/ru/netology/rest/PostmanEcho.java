eac90bd13841e08915542f7669bf43dd3d61830b
Александр Сиденов <sid_san@mail.ru>
2022-06-01 16:24:52 +0800

    JsonPath

--- src/test/java/ru/netology/rest/PostmanEcho.java
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -2,7 +2,7 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.*;
 class PostmanEcho {
     @Test
     void Test() {
@@ -10,13 +10,14 @@ class PostmanEcho {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("text/plain; charset=UTF-8")
+                .body("некоторые данные") // отправляемые данные (заголовки и query можно выставлять аналогично)
                  // Выполняемые действия
                 .when()
                 .post("/post")
                  // Проверки
                 .then()
                 .statusCode(200)
-                .body();
+                .body("data", equalTo("некоторые данные"));
     }
 }
