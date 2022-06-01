df8511fb0868b68ac517693278d83f42cec041a9
Александр Сиденов <sid_san@mail.ru>
2022-06-01 16:28:40 +0800

    fix mistake

--- src/test/java/ru/netology/rest/PostmanEcho.java
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -18,6 +18,6 @@ class PostmanEcho {
                  // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("просто данные"));
+                .body("data", equalTo("некоторые данные"));
     }
 }
